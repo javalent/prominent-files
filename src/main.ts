35086d0f060d6c3552341961da65418bed97a3f9
valentine195 <38669521+valentine195@users.noreply.github.com>
2021-09-22 12:34:03 -0400

    0.0.1
    - Initial release

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,190 @@
-// @ts-nocheck
-
-import { Plugin } from "obsidian";
+import { App, Notice, Plugin, setIcon } from "obsidian";
+import { around } from "monkey-around";
 
 import "./main.css";
 
+interface InternalPlugin {
+    enabled: boolean;
+    enable: (b: boolean) => void;
+    disable: (b: boolean) => void;
+}
+interface Starred extends InternalPlugin {
+    instance: {
+        addItem: (file: StarredFile) => void;
+        removeItem: (file: StarredFile) => void;
+        items: StarredFile[];
+    };
+}
+interface FileExplorer extends InternalPlugin {}
+
+interface StarredFile {
+    type: "file";
+    title: string;
+    path: string;
+}
+interface InternalPlugins {
+    starred: Starred;
+    "file-explorer": FileExplorer;
+}
+
+declare module "obsidian" {
+    interface App {
+        internalPlugins: {
+            plugins: InternalPlugins;
+            getPluginById<T extends keyof InternalPlugins>(
+                id: T
+            ): InternalPlugins[T];
+            loadPlugin(...args: any[]): any;
+        };
+    }
+    interface WorkspaceLeaf {
+        containerEl: HTMLElement;
+    }
+}
+
 export default class ProminentStarredFiles extends Plugin {
-    items: any[];
-    async onload(): Promise<void> {
+    handler: () => void;
+    files: Set<StarredFile> = new Set();
+    get enabled() {
+        return this.app.internalPlugins.getPluginById("starred").enabled;
+    }
+    get starred() {
+        return this.app.internalPlugins.getPluginById("starred");
+    }
+    get instance() {
+        if (!this.enabled) return;
+        return this.starred.instance;
+    }
+    get fileExplorers() {
+        return this.app.workspace.getLeavesOfType("file-explorer");
+    }
+    async onload() {
         console.log("Prominent Starred Files plugin loaded");
 
-        this.app.internalPlugins.plugins.starred.instance.items = new Proxy(
-            this.app.internalPlugins.plugins.starred.instance.items,
-            {
-                get: (target, property) => {
-                    if (
-                        property === "IDENTITY" &&
-                        !target.hasOwnProperty("IDENTITY")
-                    ) {
-                        // missing '!'
-                        return target;
-                    }
-                    // property is index in this case
-                    return target[property];
-                },
-                set: (target, property, value, receiver) => {
-                    //target[property] = value;
-                    if (value.hasOwnProperty("type")) {
-                        this.items = [...target, value];
-                        this.refreshStars();
-                    }
-                    return Reflect.set(target, property, value, receiver);
-                    // you have to return true to accept the changes
-                    //return true;
-                }
-            }
-        );
+        this.app.workspace.onLayoutReady(() => this.checkAndEnable());
     }
-    refreshStars() {
-        console.log(this.app.workspace.leftSplit);
-        let titleNodes = this.app.workspace.leftSplit.containerEl.querySelectorAll(
-            ".nav-file"
-        );
-        console.log(titleNodes);
+    checkAndEnable() {
+        setTimeout(() => {
+            const self = this;
+            if (
+                !this.app.internalPlugins.getPluginById("file-explorer").enabled
+            ) {
+                new Notice(
+                    "The File Explorer core plugin must be enabled to use this plugin."
+                );
+
+                const explorer = around(
+                    this.app.internalPlugins.getPluginById("file-explorer"),
+                    {
+                        enable: function (next) {
+                            return function (b) {
+                                const apply = next.call(this, b);
+                                explorer();
+                                self.checkAndEnable();
+                                return apply;
+                            };
+                        },
+                        disable: function (next) {
+                            return function (b) {
+                                explorer();
+                                self.checkAndEnable();
+                                return next.call(this, b);
+                            };
+                        }
+                    }
+                );
+                this.register(explorer);
+                return;
+            }
+
+            this.register(
+                around(this.starred, {
+                    enable: function (next) {
+                        return function (b) {
+                            const apply = next.call(this, b);
+                            self.registerHandlers();
+                            for (let item of self.instance?.items ?? []) {
+                                self.applyStar(item);
+                            }
+                            return apply;
+                        };
+                    },
+                    disable: function (next) {
+                        return function (b) {
+                            self.handler();
+                            for (let item of self.instance?.items ?? []) {
+                                self.removeStar(item);
+                            }
+                            return next.call(this, b);
+                        };
+                    }
+                })
+            );
+            if (!this.enabled) {
+                new Notice(
+                    "The Starred core plugin must be enabled to use this plugin."
+                );
+            } else {
+                this.registerHandlers();
+            }
+        });
+    }
+    registerHandlers() {
+        const self = this;
+        for (let item of this.instance?.items ?? []) {
+            this.applyStar(item);
+        }
+
+        this.handler = around(this.starred.instance, {
+            addItem: function (next) {
+                return function (file) {
+                    self.applyStar(file);
+                    return next.call(this, file);
+                };
+            },
+            removeItem: function (next) {
+                return function (file) {
+                    self.removeStar(file);
+                    return next.call(this, file);
+                };
+            }
+        });
+        this.register(this.handler);
+    }
+    applyStar(file: StarredFile) {
+        if (!this.fileExplorers.length) return;
+        if (this.files.has(file)) return;
+
+        this.files.add(file);
+
+        for (let explorer of this.fileExplorers) {
+            const element = explorer.containerEl.querySelector(
+                `.nav-file-title[data-path="${file.path}"]`
+            );
+            if (!element) continue;
+
+            setIcon(element.createDiv("prominent-star"), "star-glyph");
+        }
+    }
+    removeStar(file: StarredFile) {
+        if (!this.fileExplorers.length) return;
+
+        this.files.delete(file);
+
+        for (let explorer of this.fileExplorers) {
+            const element = explorer.containerEl.querySelector(
+                `.nav-file-title[data-path="${file.path}"]`
+            );
+            if (!element) continue;
+
+            const stars = element.querySelectorAll(".prominent-star");
+            if (stars.length) stars.forEach((star) => star.detach());
+        }
     }
     onunload() {
         console.log("Prominent Starred Files plugin unloaded");
-        this.app.internalPlugins.plugins.starred.instance.items = this.app.internalPlugins.plugins.starred.instance.items.IDENTITY;
+        for (let file of this.instance?.items ?? []) {
+            this.removeStar(file);
+        }
     }
 }
